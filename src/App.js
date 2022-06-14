b7ccc190c17b4162ff7f981298665515fbb7059c
Pau <pauligonzalezincker@gmail.com>
2022-06-14 15:08:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+  console.log('component has rendered!');
+  
+
+  //once the component has renders, I will run a side effect: fetching data
+
+  useEffect(() => {
+    //api call within a useEffect to hold my data
+    axios({
+      baseURL: 'http://gateway.marvel.com/',
+      url: 'v1/public/characters',
+      method: 'GET',
+      params: {
+        ts: 'thesoer',
+        apikey: 'f6a569770dbc42c8e49153f86a614843',
+        hash: '2b386c7f05318a6e66fed6f3416c4b2d',
+       
+      }
+    }).then((data) => {
+      console.log(data.data.data.results);
+      
+
+    })
+  })
+  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>If I were a superheroe...</h1>
       </header>
     </div>
   );
