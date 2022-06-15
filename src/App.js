b8ed85f4a9bb595cb0140ef60c85d71a49115db6
Pau <pauligonzalezincker@gmail.com>
2022-06-15 17:55:25 -0400

    biuld folder created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react'; 
+import Landing from './Landing';
 
 function App() {
   console.log('component has rendered!');
+
+  //call useState hook to hold the array back from my api - destructure variable and function 
+  const [superHeroPics, setSuperHeroPics] = useState([]); //expecting an array of objects, so I initialize with empty one
   
 
   //once the component has renders, I will run a side effect: fetching data
@@ -18,22 +22,22 @@ function App() {
         ts: 'thesoer',
         apikey: 'f6a569770dbc42c8e49153f86a614843',
         hash: '2b386c7f05318a6e66fed6f3416c4b2d',
-       
+        nameStartsWith: 'sp', //update/link this according to user's choice
+        limit: 20,  
       }
-    }).then((data) => {
-      console.log(data.data.data.results);
-      
+    }).then((apiData) => {
+      console.log(apiData.data.data.results);
 
+      //store data returned from api in state
+      setSuperHeroPics(apiData.data.data.results)
     })
-  })
+  }, [])
   
   
 
   return (
     <div className="App">
-      <header>
-        <h1>If I were a superheroe...</h1>
-      </header>
+     <Landing />
     </div>
   );
 }
---
+++ src/Landing.js
@@ -0,0 +1,13 @@
+const Landing = () => {
+    return (
+        <header>
+        {/* <h1>If I had superpowers...</h1> */}
+        <button>SEARCH</button>
+        <button>AVENGERS</button>
+
+      </header>
+
+    )
+}
+
+export default Landing;
